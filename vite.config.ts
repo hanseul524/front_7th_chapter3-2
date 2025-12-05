85c4c3c889a6d2f58bfe08cb37656ea8934d93e4
hanseul jung <hanseul56524@gmail.com>
2025-12-06 01:32:16 +0900

    feat: 배포관련 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: process.env.VITE_BASE_PATH || '/',
+    build: {
+      rollupOptions: {
+        input: ['./index.advanced.html', './index.basic.html']
+      },
+      outDir: 'dist'
+    }
   }),
   defineTestConfig({
     test: {
